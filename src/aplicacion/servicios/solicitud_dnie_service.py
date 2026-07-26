35232182699b2f29eaa8be362effc1e5c7c22bb1
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2026-07-26 13:33:01 -0500

    feat(aplicacion): implementar lógica de servicio para listar solicitudes

--- src/aplicacion/servicios/solicitud_dnie_service.py
+++ src/aplicacion/servicios/solicitud_dnie_service.py
@@ -1,3 +1,13 @@
+from typing import List, Optional
+
 from src.aplicacion.interfaces.i_solicitud_dnie_service import ISolicitudDNIeService
 from src.dominio.modelo.solicitud_dnie import SolicitudDNIe
 from src.dominio.repositorio.i_solicitud_dnie_repository import ISolicitudDNIeRepository
+
+class SolicitudDNIeService(ISolicitudDNIeService):
+
+    def __init__(self, repositorio: ISolicitudDNIeRepository):
+        self._repositorio = repositorio
+
+    def listar_solicitudes(self) -> List[SolicitudDNIe]:
+        return self._repositorio.listar()
